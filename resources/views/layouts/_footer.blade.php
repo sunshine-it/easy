8b1315fb4e6dbaf4f698dbe83c96c8ae7bb6fa46
vagrant <vagrant@homestead>
2019-07-23 09:50:21 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+  <span style="color: #e27575;font-size: 14px;">❤</span>
+  <a href="https://easy-wb.herokuapp.com/" target="_blank">学以致用，每日精进</a>
+  <div class="float-right">
+      <a href="#">关于</a>
+  </div>
+</footer>
