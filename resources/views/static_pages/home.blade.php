da64f4b04a5a8b9014ba748eace030542c4e533a
vagrant <vagrant@homestead>
2019-07-23 09:17:44 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title', '首页')
 
 @section('content')
-<h1>Home-首页</h1>
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      Home-首页
+    </p>
+    <p>
+    一切，将从这里开始。
+    </p>
+    <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+</div>
 @stop
