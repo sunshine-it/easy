71c5fced302da0f0b16fdb6b6211a210283f0663
vagrant <vagrant@homestead>
2019-07-23 10:15:06 +0000

    用户注册页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// 用户类
+class UsersController extends Controller
+{
+    // 注册
+    public function create()
+    {
+        return view('users.create');
+    }
+}
