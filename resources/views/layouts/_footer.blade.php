69ccbe8370df1fca0ea95c8b02a7dc67d9a22720
vagrant <vagrant@homestead>
2019-07-23 10:04:53 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
   <span style="color: #e27575;font-size: 14px;">❤</span>
   <a href="https://easy-wb.herokuapp.com/" target="_blank">学以致用，每日精进</a>
   <div class="float-right">
-      <a href="#">关于</a>
+      <a href="{{ route('about') }}">关于</a>
   </div>
 </footer>
