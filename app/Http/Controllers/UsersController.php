0dffef9e694df7dbc13cd4d6eb90eacf400d20e1
vagrant <vagrant@homestead>
2019-07-24 02:25:57 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,8 +17,18 @@ class UsersController extends Controller
     // 显示用户的信息
     public function show(User $user)
     {
-        // dd($user);
-        // dd(compact('user'));
         return view('users.show', compact('user'));
+    }
+
+    // 处理用户注册信息
+    public function store(Request $request)
+    {
+        // 验证规则
+        $this->validate($request, [
+            'name'      => 'required|max:50',
+            'email'     => 'required|email|unique:users|max:255',
+            'password'  => 'required|confirmed|min:6' // 保证两次输入的密码一致，可以使用 confirmed 来进行密码匹配验证。
+        ]);
+        return;
     }
 }
