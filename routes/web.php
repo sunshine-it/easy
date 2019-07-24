28a76addc9f5df8038c11236d57d7e42de8edd58
vagrant <vagrant@homestead>
2019-07-24 04:16:08 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 // 用户资源路由
 Route::resource('users', 'UsersController');
+// 访问登录页面，输入账号密码点击登录路由；
+Route::get('login', 'SessionsController@create')->name('login');
+// 服务器对用户身份进行认证，认证通过后，记录登录状态并进行页面重定向；
+Route::post('login', 'SessionsController@store')->name('login');
+// 登录成功后的用户，能够使用退出按钮来销毁当前登录状态；
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
