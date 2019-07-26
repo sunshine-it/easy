447b60c1310eff6fc404eb68bcbbef2eccf12ecc
vagrant <vagrant@homestead>
2019-07-26 11:59:25 +0000

    微博数据模型

---
+++ app/Models/Status.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+// 微博模型
+class Status extends Model
+{
+    // 一条微博属于一个用户
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Str;
 
+// 用户模型
 class User extends Authenticatable
 {
     use Notifiable;
@@ -28,6 +29,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // 指明一个用户拥有多条微博
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 
     // 用户模型监听器
     public static function boot()
