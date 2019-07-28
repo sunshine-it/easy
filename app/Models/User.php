0c4b44a669bcc0d1971f3356df72152a1fbcfe84
vagrant <vagrant@homestead>
2019-07-28 08:21:33 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Str;
+use Auth;
 
 // 用户模型
 class User extends Authenticatable
@@ -61,10 +62,14 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
-    // feed 方法获取当前用户关注的人发布过的所有微博动态
+    // feed 方法获取当前用户关注的人发布过的所有微博动态 简称 动态流
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        // 将当前用户的 id 加入到 user_ids 数组中
+        array_push($user_ids, $this->id);
+        // 使用查询构造器 whereIn 方法取出所有用户的微博动态并进行倒序排序，使用预加载 with 方法，预加载避免了 N+1 查找的问题
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 
     // 获取粉丝关系列表
